a9ed6691f4872007ad494e012b61b5740424d996
Cyanne Jones <jonescyanne@yahoo.com>
2025-03-26 19:22:43 -0600

    Add update value to store

--- src/useFavoriteStore.ts
+++ src/useFavoriteStore.ts
@@ -13,6 +13,7 @@ interface FavoriteStore {
   favorites: Favorite[];
   addFavorite: (favorite: Favorite) => void;
   removeFavorite: (url: string) => void;
+  updateFavorite: (url: string, newValues: Omit<Favorite, 'url'>) => void;
 }
 
 const useFavoriteStore = create<FavoriteStore>((set) => ({
@@ -24,6 +25,16 @@ const useFavoriteStore = create<FavoriteStore>((set) => ({
   }),
   removeFavorite: (url) => set((state) => {
     const newFavorites = state.favorites.filter((favorite) => favorite.url !== url);
+    localStorage.setItem('favorites', JSON.stringify(newFavorites));
+    return { favorites: newFavorites };
+  }),
+  updateFavorite: (url, newValues) => set((state) => {
+    const newFavorites = state.favorites.map((favorite) => {
+      if (favorite.url === url) {
+        return { ...favorite, ...newValues };
+      }
+      return favorite;
+    });
     localStorage.setItem('favorites', JSON.stringify(newFavorites));
     return { favorites: newFavorites };
   }),
